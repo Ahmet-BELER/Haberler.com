24e5d6fa81204c463934ac0a17bcf6c49b47bbb6
Ahmet <ahmetbeler1739@gmail.com>
2023-12-24 22:07:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleNavbar() {
+    var navbarLinks = document.getElementById("navbarLinks");
+    navbarLinks.style.display = (navbarLinks.style.display === "flex") ? "none" : "flex";
+  }
+  function toggleNavbar2() {
+    var navbarLinks = document.getElementById("navbarLinks2");
+    navbarLinks.style.display = (navbarLinks.style.display === "flex") ? "none" : "flex";
+  }  
+  
